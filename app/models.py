04354525644047ca583bc4f37d43bd48566f410c
Oliver Harvey <oliharvey@hotmail.com>
2022-04-15 20:19:30 +0100

    log in out system

--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
 from enum import unique
 from datetime import datetime
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -12,6 +15,12 @@ class User(db.Model):
     def __repr__(self):
         return f'<User ({self.username})>'
 
+    def set_password(self, password):
+        self.passoword_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.passoword_hash, password)
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -20,3 +29,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
